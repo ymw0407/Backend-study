b552b812c96c7e00806e220b28e0185154eefac2
Eonji-sw <85453429+Eonji-sw@users.noreply.github.com>
2023-01-27 00:29:23 +0900

    sparkles [2주차][Feat] Node.js(2) 완성 #8

---
+++ eonji-sw/week-2/Node.js(2)/02-04-welcome-template-destructuring-2/index.js
@@ -0,0 +1,31 @@
+// Node.js로 이메일 템플릿 생성하기 - Shorthand property names
+
+function getWelcomeTemplate({ name, age, school, createdAt }) {
+    return `
+        <html>
+            <body>
+                <h1>${name}님 가입을 환영합니다.</h1>
+                <hr />
+                <div>이름: ${name}</div>
+                <div>나이: ${age}살</div>
+                <div>학교: ${school}</div>
+                <div>가입일: ${createdAt}</div>
+            </body>
+        </html>
+    `;
+}
+
+const name = '철수';
+const age = 13;
+const school = '다람쥐초등학교';
+const createdAt = '2010-09-07';
+
+// const myUser = {
+//     name,
+//     age,
+//     school,
+//     createdAt,
+// };
+
+const result = getWelcomeTemplate({ name, age, school, createdAt });
+console.log(result);
---
+++ eonji-sw/week-2/Node.js(2)/02-05-welcome-template-api/email.js
@@ -0,0 +1,32 @@
+// Node.js로 회원가입 환영 이메일 함수 만들기
+
+import { getToday } from "./utils.js";
+
+export function checkValidationEmail(email) {
+    if (email === undefined || !email.includes('@')) {
+        console.log('정확한 이메일 주소를 입력해주세요.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export function getWelcomeTemplate({ name, age, school }) {
+    return `
+        <html>
+            <body>
+                <h1>${name}님 가입을 환영합니다.</h1>
+                <hr />
+                <div>이름: ${name}</div>
+                <div>나이: ${age}살</div>
+                <div>학교: ${school}</div>
+                <div>가입일: ${getToday()}</div>
+            </body>
+        </html>
+    `;
+}
+
+export function sendWelcomeTemplateToEmail(email, template) {
+    // 템플릿을 이메일에 전송
+    console.log(`${email}로 템플릿 ${template}를 전송합니다.`)
+}
