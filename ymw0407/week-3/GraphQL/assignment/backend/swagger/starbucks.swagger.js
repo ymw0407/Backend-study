6476fff74dfa6f566397e8e166408ada5f969133
ymw0407 <yunminwo1211@gmail.com>
2023-02-09 01:54:13 +0900

    :sparkles: [3주차][Feat] GraphQL 과제 완료 #16

---
+++ ymw0407/week-3/GraphQL/assignment/backend/swagger/starbucks.swagger.js
@@ -0,0 +1,35 @@
+// starbucks.swagger.js
+
+/**
+ * @swagger
+ * /starbucks:
+ *   get:
+ *     summary: 커피 메뉴 리스트 가져오기
+ *     tags: [Starbucks]
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         content:
+ *           application/json:
+ *              schema:
+ *                  type: array
+ *                  items:
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: 아메리카노
+ *                          kcal:
+ *                              type: number
+ *                              example: 5
+ */
+
+/**
+ * @swagger
+ * /starbucks:
+ *   get:
+ *     summary: 커피 메뉴 리스트 가져오기
+ *     tags: [Starbucks]
+ *     responses:
+ *          200:
+ *              description: 성공
+ */
